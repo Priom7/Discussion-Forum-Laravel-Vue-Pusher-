13549e3ff975a68449488119557790439c41e5fa
Md. Sharif Alam <priom7197@gmail.com>
2020-09-18 20:12:36 +0600

    Fectory Seeding done

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -4,9 +4,13 @@
 
 use App\Model\Category;
 use Faker\Generator as Faker;
+use PhpParser\Node\Expr\Cast\String_;
 
 $factory->define(Category::class, function (Faker $faker) {
+    $word = $faker->word;
     return [
         //
+        'name' => $word,
+        'slug' => Str::slug($word)
     ];
 });
--- database/factories/ReplyFactory.php
+++ database/factories/ReplyFactory.php
@@ -2,11 +2,22 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\Model\Post;
 use App\Model\Reply;
+use App\User;
 use Faker\Generator as Faker;
 
 $factory->define(Reply::class, function (Faker $faker) {
     return [
         //
+        'body' => $faker->text,
+        'post_id' => function () {
+            return Post::all()->random();
+        },
+        'user_id' => function () {
+            return User::all()->random();
+        },
+
+
     ];
 });
