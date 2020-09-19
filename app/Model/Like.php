200c7231ea9f44eba30ab011a01e3a48d031d8b3
Md. Sharif Alam <priom7197@gmail.com>
2020-09-19 12:36:56 +0600

    Likes and Dislikes added

--- app/Model/Like.php
+++ app/Model/Like.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Like extends Model
 {
     //
+    protected $guarded = [];
 }
--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,5 @@ use App\Model\Category;
 Route::apiResource('/post', 'PostController');
 Route::apiResource('/category', 'CategoryController');
 Route::apiResource('/post/{post}/reply', 'ReplyController');
+Route::post('/{reply}/like', 'LikeController@likeIt');
+Route::delete('/{reply}/like', 'LikeController@unlikeIt');
