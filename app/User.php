3241449f6756de762525b66f79fa23012a815c0d
Md. Sharif Alam <priom7197@gmail.com>
2020-09-19 18:41:38 +0600

    Sign Up done

--- app/User.php
+++ app/User.php
@@ -55,4 +55,9 @@ class User extends Authenticatable implements JWTSubject
     {
         return [];
     }
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password']  = bcrypt($value);
+    }
 }
